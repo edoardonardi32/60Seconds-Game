de774d519b2d11f434fba190c636dd183c645338
edoardonardi <nardi.edoardo@franchettisalviani.ent>
2026-03-10 13:31:42 +0100

    Implementazione di qualche metodo in eventi interfaccia iniziale con collegamentoi ad un'altra form funzionante

--- 60Seconds-Game/src/pkg60seconds/game/Eventi.java
+++ 60Seconds-Game/src/pkg60seconds/game/Eventi.java
@@ -10,10 +10,30 @@ package pkg60seconds.game;
  */
 public class Eventi {
 
-    public void generaInfestazione() {
+    public String generaInfestazione() {
+        String[] messaggi = {
+        "Oh no! La cucina è invasa dagli scarafaggi!",
+        "Attenzione! Gli scarafaggi hanno preso il sopravvento!",
+        "Un terribile sciame di scarafaggi corre sulle pareti!",
+        "Scorri più veloce! Gli scarafaggi stanno ovunque!",
+        "Che disgrazia! La tua casa è infestata da scarafaggi!"
+    };
+    
+    int indice = (int) (Math.random() * messaggi.length);
+    return messaggi[indice];
     }
 
-    public void eliminaInfestazione() {
+    public String eliminaInfestazione() {
+        String[] messaggi = {
+        "Missione compiuta! Nessuno scarafaggio rimane, la cucina è finalmente sicura.",
+        "Gli ultimi scarafaggi sono stati annientati! La vittoria è tua!",
+        "Casa sicura e pulita! Non rimane nemmeno un insetto.",
+        "Trionfo totale! L’infestazione è finita e la tua casa è finalmente libera.",
+        "Hai liberato completamente la casa dagli scarafaggi! Che sollievo!"
+    };
+    
+    int indice = (int) (Math.random() * messaggi.length);
+    return messaggi[indice];
     }
 
     public void perditaScorte() {
--- 60Seconds-Game/src/pkg60seconds/game/Game.java
+++ 60Seconds-Game/src/pkg60seconds/game/Game.java
@@ -14,7 +14,8 @@ public class Game {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Interfaccia i = new Interfaccia();
+        i.setVisible(true);
     }
     
 }
